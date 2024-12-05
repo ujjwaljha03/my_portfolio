29913f2dc0e37b1cb1885a90aad111c72d67e94c
Ujjwal Jha <128184072+ujjwaljha03@users.noreply.github.com>
2024-12-06 00:37:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
-import { join } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
 import forms from "@tailwindcss/forms";
 
+// Manually define __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default {
-  content: [join(__dirname, "src/**/*.{html,js,jsx,ts,tsx}")],
+  content: [path.join(__dirname, "src/**/*.{html,js,jsx,ts,tsx}")],
   darkMode: "class",
   theme: {
     extend: {},
